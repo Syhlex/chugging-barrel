9b0498fe4cdeeef9e22f108381a04dff78ea99b6
Ian Zhang <ianzh4ng@gmail.com>
2024-10-16 22:31:18 -0700

    Rename example files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Example
-An example greeter plugin
+# Chugging Barrel
+
+Adds loadout names to the chugging barrel.
--- src/main/java/com/chuggingbarrel/ChuggingBarrelPlugin.java
+++ src/main/java/com/chuggingbarrel/ChuggingBarrelPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.chuggingbarrel;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Chugging Barrel"
 )
-public class ExamplePlugin extends Plugin
+public class ChuggingBarrelPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ChuggingBarrelConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ChuggingBarrelConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ChuggingBarrelConfig.class);
 	}
 }
