76122f8a4d05e4711944ba8f4673a95f953c1beb
Ian Zhang <ianzh4ng@gmail.com>
2025-10-02 01:27:23 -0700

    Issue-5: Update widget IDs after update

--- src/main/java/com/chuggingbarrel/ChuggingBarrelConstants.java
+++ src/main/java/com/chuggingbarrel/ChuggingBarrelConstants.java
@@ -7,18 +7,18 @@ public class ChuggingBarrelConstants {
     public static final int CHUGGING_BARREL_VARBIT_VALUE_ON = 5;
     public static final int CHUGGING_BARREL_VARBIT_VALUE_OFF = 0;
 
-    public static final int LOADOUTS_SCROLLBAR_ID = 983092;
-    public static final int LOADOUTS_SCROLL_CONTAINER_ID = 983093;
-    public static final int[] LOADOUT_CONTAINER_IDS = {983094, 983095, 983096, 983097};
-    public static final int[] POTION_CONTAINER_IDS = {983109, 983100, 983103, 983106};
-    public static final int[] LOAD_BUTTON_IDS = {983107, 983098, 983101, 983104};
-    public static final int DEPOSIT_POTIONS_ID = 983082;
+    public static final int LOADOUTS_SCROLLBAR_ID = 983111;
+    public static final int LOADOUTS_SCROLL_CONTAINER_ID = 983112;
+    public static final int[] LOADOUT_CONTAINER_IDS = {983113, 983114, 983115, 983116};
+    public static final int[] POTION_CONTAINER_IDS = {983128, 983119, 983122, 983125};
+    public static final int[] LOAD_BUTTON_IDS = {983126, 983117, 983120, 983123};
+    public static final int DEPOSIT_POTIONS_ID = 983101;
 
     public static final int TITLE_WIDGET_HEIGHT = 20;
 
     public static final int OVERLAY_CHAR_LIMIT = 7;
 
-    public static final int BANK_CURRENT_POTIONS_ID = 983090;
+    public static final int BANK_CURRENT_POTIONS_ID = 983109;
     public static final int INVENTORY_CURRENT_POTIONS_ID = 58064903;
 
     public static final int BARREL_BANK_INTERFACE_DOSE_CHANGE_SCRIPT = 7355;
